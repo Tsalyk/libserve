d792ecce61e2a7ec33dbbc97ded60b4375ca228b
Tsalyk <zalukm1@gmail.com>
2021-03-13 17:51:06 +0200

    add client part

---
+++ client.py
@@ -0,0 +1,25 @@
+import socket, time
+from msgutils import sendMsg, readMsg
+
+def launch():
+    """
+    Launches client's side
+    """
+    sock = socket.socket()
+    sock.connect(('localhost', 12345))
+
+    name = 'markiian'
+    sendMsg(name.encode('866'), sock)
+
+    surname = input("Enter your surname: ")
+    time.sleep(0.1)
+    sendMsg(surname.encode('866'), sock)
+
+    age = input("Enter your age: ")
+    time.sleep(0.1)
+    sendMsg(age.encode('866'), sock)
+
+
+    sock.close()
+
+launch()
